8ca886ae1762fe50eb72ce0afea85d33c8e7a1ce
fahim <muhdfahim786@gamil.com>
2026-04-21 22:25:48 +0530

    add redux persist

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { persistor, store } from "./redux/store.js";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {store} from "./redux/store.js"
-import {Provider} from "react-redux"
-
-createRoot(document.getElementById('root')).render(
-<Provider store={store}>
-  <App />
-</Provider>
-    
-
-)
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
+);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,59 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from "./user/userSlice"
+// import { combineReducers, configureStore } from '@reduxjs/toolkit'
+// import userReducer from "./user/userSlice"
+// import storage from 'redux-persist/lib/storage'
+// import {persistReducer, persistStore} from 'redux-persist'
+
+// const rootReducer= combineReducers({user:userReducer})
+
+
+// const persistConfig={
+//   key:"root",
+//   storage,
+//   version:1
+// }
+// const persistedReducer=persistReducer(persistConfig,rootReducer)
+// export const store = configureStore({
+//   reducer: persistedReducer
+//   ,
+//   middleware:(getDefaultMiddleware)=>
+//     getDefaultMiddleware({
+//     serializableCheck:false,
+
+//   }),
+// })
+
+// export const persistor=persistStore(store)
+
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import userReducer from "./user/userSlice";
+import { persistReducer, persistStore } from "redux-persist";
+
+
+// ✅ SAFE STORAGE (manual)
+const storage = {
+  getItem: (key) => Promise.resolve(localStorage.getItem(key)),
+  setItem: (key, value) => Promise.resolve(localStorage.setItem(key, value)),
+  removeItem: (key) => Promise.resolve(localStorage.removeItem(key)),
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version:1
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user:userReducer
-  },
-  middleware:(getDefaultMiddleware)=>
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-    serializableCheck:false,
+      serializableCheck: false,
+    }),
+});
 
-  }),
-})
+export const persistor = persistStore(store);
